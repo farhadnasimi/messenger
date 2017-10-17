8afed2f96656b51e0fa2a63bbd3033abc0053c85
fnasimi <farhad.nasimi@gmail.com>
2017-10-16 19:29:42 -0500

    ProfileResource class added

--- src/main/java/com/farhad/rest/messenger/resources/ProfileResource.java
+++ src/main/java/com/farhad/rest/messenger/resources/ProfileResource.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -14,6 +16,7 @@ import com.farhad.rest.messenger.services.ProfileService;
 @Path("/profiles")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
+
 public class ProfileResource {
 	
 	ProfileService ps = new ProfileService();
@@ -21,5 +24,16 @@ public class ProfileResource {
 	@GET
 	public List<Profile> getProfiles(){
 		return ps.getProfiles();
+	}
+	
+	@POST
+	public Profile addProfile(Profile profile){
+		return ps.addProfile(profile);
+	}
+	
+	@GET
+	@Path("/{ProfileName}")
+	public Profile getProfile(@PathParam("ProfileName") String profile){
+		return ps.getProfile(profile);
 	}
 }
--- src/main/java/com/farhad/rest/messenger/services/ProfileService.java
+++ src/main/java/com/farhad/rest/messenger/services/ProfileService.java
@@ -1,5 +1,6 @@
 package com.farhad.rest.messenger.services;
 import java.util.*;
+
 import com.farhad.rest.messenger.database.DatabaseClass;
 import com.farhad.rest.messenger.model.Profile;
 
@@ -14,5 +15,27 @@ public class ProfileService {
 	
 	public List<Profile> getProfiles(){
 		return new ArrayList<Profile>(profiles.values());
+	}
+	
+	public Profile getProfile(String profile){
+		return profiles.get(profile);
+	}
+	
+	public Profile addProfile(Profile profile){
+		profile.setId(profiles.size() + 1);
+	    profiles.put(profile.getProfileName(), profile);
+		return profile;
+	}
+	
+	public Profile updateProfile(Profile profile){
+		if(profile.getId() <= 0){
+			return null;
+		}
+		profiles.put(profile.getProfileName(), profile);
+		return profile;
+	}
+	
+	public void removeProfile(Profile profile){
+		profiles.remove(profile);
 	}
 }
